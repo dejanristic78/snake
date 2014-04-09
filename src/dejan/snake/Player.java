d9f5ba882b6ebeb2fda2e44cd775630f70c2b98c
Dejan <Dejan@130.229.171.154>
2014-04-09 14:51:58 +0200

    asdf

--- src/dejan/snake/Player.java
+++ src/dejan/snake/Player.java
@@ -9,21 +9,24 @@ import javafx.scene.paint.Color;
 public class Player extends GameEntity implements EventHandler<KeyEvent>{
     private class BodySegment{
         private BodySegment nextSegment;
-        private final int segmentNr;
+        private int segmentNr;
         private int xPos;
         private int yPos;
         public BodySegment(int xPos, int yPos, int segmentNr) {
             this.xPos = xPos;
             this.yPos = yPos;
             this.segmentNr = segmentNr;
+            
             if(segmentNr < bodyLength) {
                 nextSegment = new BodySegment(xPos, yPos, ++segmentNr);
             }
             else {
                 nextSegment = null;
             }
-            
         }
+        
+        
+        
         public void update(int xPos, int yPos) {
             if(nextSegment == null) {
                 canvas.drawBlock(this.xPos, this.yPos, Color.WHITE);
@@ -34,27 +37,35 @@ public class Player extends GameEntity implements EventHandler<KeyEvent>{
             
             this.xPos = xPos;
             this.yPos = yPos;
-            canvas.drawBlock(this.xPos, this.yPos, Color.BLUE);
+            canvas.drawBlock(this.xPos, this.yPos, COLOR);
         }
         
     }
+    
+    private final static int INIT_SIZE = 3;
+    private final static int GROW_SIZE = 3;
+    private final static Color COLOR = Color.BLUE;
     
     private BodySegment head;
     private int bodyLength = 3;
 
     public static enum Direction{ UP, DOWN, LEFT, RIGHT }
-    private Direction moveDirection = Direction.LEFT;
+    private Direction moveDirection;
+    private Direction nextMoveDirection = Direction.RIGHT;
     
     private int xPos = 5;
     private int yPos = 5;
     
+    private Food food;
 
-    public Player(GameCanvas canvas) {
+    public Player(GameCanvas canvas, Food food) {
         super(canvas);
         head = new BodySegment(xPos, yPos, 0);
+        this.food = food;
     }
     @Override
     public void update() {
+        moveDirection = nextMoveDirection;
         switch(moveDirection) {
             case UP:    yPos--;     break;
             case DOWN:  yPos++;     break;
@@ -62,6 +73,10 @@ public class Player extends GameEntity implements EventHandler<KeyEvent>{
             case RIGHT: xPos++;     break;
         }
         head.update(xPos, yPos);
+        
+        if( food.foundItem(this) ) {
+            bodyLength += GROW_SIZE;
+        }
     }
     @Override
     public void handle(KeyEvent event) {
@@ -69,20 +84,29 @@ public class Player extends GameEntity implements EventHandler<KeyEvent>{
         switch(keyCode) {
             case UP:
                 if(moveDirection != Direction.DOWN)
-                    moveDirection = Direction.UP;       
+                    nextMoveDirection = Direction.UP;       
                 break;
             case DOWN:  
                 if(moveDirection != Direction.UP)
-                    moveDirection = Direction.DOWN;     
+                    nextMoveDirection = Direction.DOWN;     
                 break;
             case LEFT:  
                 if(moveDirection != Direction.RIGHT)
-                    moveDirection = Direction.LEFT;     
+                    nextMoveDirection = Direction.LEFT;     
                 break;
             case RIGHT: 
                 if(moveDirection != Direction.LEFT)
-                    moveDirection = Direction.RIGHT;    
+                    nextMoveDirection = Direction.RIGHT;    
                 break;
         }
     }
+
+    public int getXPos() {
+        return xPos;
+    }
+
+    public int getYPos() {
+        return yPos;
+    }
+    
 }
